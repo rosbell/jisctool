1ca9fabe8c2d081ae8d8c89a904deb1c707b6481
Ros <ros.bell@manchester.ac.uk>
2021-08-20 16:04:19 +0100

    trying to git

---
+++ main/assets/js/scripts.js
@@ -0,0 +1,570 @@
+// Utility function
+function Util () {};
+
+/* 
+	class manipulation functions
+*/
+Util.hasClass = function(el, className) {
+	if (el.classList) return el.classList.contains(className);
+	else return !!el.getAttribute('class').match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+};
+
+Util.addClass = function(el, className) {
+	var classList = className.split(' ');
+ 	if (el.classList) el.classList.add(classList[0]);
+  else if (!Util.hasClass(el, classList[0])) el.setAttribute('class', el.getAttribute('class') +  " " + classList[0]);
+ 	if (classList.length > 1) Util.addClass(el, classList.slice(1).join(' '));
+};
+
+Util.removeClass = function(el, className) {
+	var classList = className.split(' ');
+	if (el.classList) el.classList.remove(classList[0]);	
+	else if(Util.hasClass(el, classList[0])) {
+		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+    el.setAttribute('class', el.getAttribute('class').replace(reg, ' '));
+	}
+	if (classList.length > 1) Util.removeClass(el, classList.slice(1).join(' '));
+};
+
+Util.toggleClass = function(el, className, bool) {
+	if(bool) Util.addClass(el, className);
+	else Util.removeClass(el, className);
+};
+
+Util.setAttributes = function(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+};
+
+/* 
+  DOM manipulation
+*/
+Util.getChildrenByClassName = function(el, className) {
+  var children = el.children,
+    childrenByClass = [];
+  for (var i = 0; i < el.children.length; i++) {
+    if (Util.hasClass(el.children[i], className)) childrenByClass.push(el.children[i]);
+  }
+  return childrenByClass;
+};
+
+Util.is = function(elem, selector) {
+  if(selector.nodeType){
+    return elem === selector;
+  }
+
+  var qa = (typeof(selector) === 'string' ? document.querySelectorAll(selector) : selector),
+    length = qa.length,
+    returnArr = [];
+
+  while(length--){
+    if(qa[length] === elem){
+      return true;
+    }
+  }
+
+  return false;
+};
+
+/* 
+	Animate height of an element
+*/
+Util.setHeight = function(start, to, element, duration, cb, timeFunction) {
+	var change = to - start,
+	    currentTime = null;
+
+  var animateHeight = function(timestamp){  
+    if (!currentTime) currentTime = timestamp;         
+    var progress = timestamp - currentTime;
+    if(progress > duration) progress = duration;
+    var val = parseInt((progress/duration)*change + start);
+    if(timeFunction) {
+      val = Math[timeFunction](progress, start, to - start, duration);
+    }
+    element.style.height = val+"px";
+    if(progress < duration) {
+        window.requestAnimationFrame(animateHeight);
+    } else {
+    	if(cb) cb();
+    }
+  };
+  
+  //set the height of the element before starting animation -> fix bug on Safari
+  element.style.height = start+"px";
+  window.requestAnimationFrame(animateHeight);
+};
+
+/* 
+	Smooth Scroll
+*/
+
+Util.scrollTo = function(final, duration, cb, scrollEl) {
+  var element = scrollEl || window;
+  var start = element.scrollTop || document.documentElement.scrollTop,
+    currentTime = null;
+
+  if(!scrollEl) start = window.scrollY || document.documentElement.scrollTop;
+      
+  var animateScroll = function(timestamp){
+  	if (!currentTime) currentTime = timestamp;        
+    var progress = timestamp - currentTime;
+    if(progress > duration) progress = duration;
+    var val = Math.easeInOutQuad(progress, start, final-start, duration);
+    element.scrollTo(0, val);
+    if(progress < duration) {
+      window.requestAnimationFrame(animateScroll);
+    } else {
+      cb && cb();
+    }
+  };
+
+  window.requestAnimationFrame(animateScroll);
+};
+
+/* 
+  Focus utility classes
+*/
+
+//Move focus to an element
+Util.moveFocus = function (element) {
+  if( !element ) element = document.getElementsByTagName("body")[0];
+  element.focus();
+  if (document.activeElement !== element) {
+    element.setAttribute('tabindex','-1');
+    element.focus();
+  }
+};
+
+/* 
+  Misc
+*/
+
+Util.getIndexInArray = function(array, el) {
+  return Array.prototype.indexOf.call(array, el);
+};
+
+Util.cssSupports = function(property, value) {
+  if('CSS' in window) {
+    return CSS.supports(property, value);
+  } else {
+    var jsProperty = property.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase();});
+    return jsProperty in document.body.style;
+  }
+};
+
+// merge a set of user options into plugin defaults
+// https://gomakethings.com/vanilla-javascript-version-of-jquery-extend/
+Util.extend = function() {
+  // Variables
+  var extended = {};
+  var deep = false;
+  var i = 0;
+  var length = arguments.length;
+
+  // Check if a deep merge
+  if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+    deep = arguments[0];
+    i++;
+  }
+
+  // Merge the object into the extended object
+  var merge = function (obj) {
+    for ( var prop in obj ) {
+      if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+        // If deep merge and property is an object, merge properties
+        if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+          extended[prop] = extend( true, extended[prop], obj[prop] );
+        } else {
+          extended[prop] = obj[prop];
+        }
+      }
+    }
+  };
+
+  // Loop through each object and conduct a merge
+  for ( ; i < length; i++ ) {
+    var obj = arguments[i];
+    merge(obj);
+  }
+
+  return extended;
+};
+
+// Check if Reduced Motion is enabled
+Util.osHasReducedMotion = function() {
+  if(!window.matchMedia) return false;
+  var matchMediaObj = window.matchMedia('(prefers-reduced-motion: reduce)');
+  if(matchMediaObj) return matchMediaObj.matches;
+  return false; // return false if not supported
+}; 
+
+/* 
+	Polyfills
+*/
+//Closest() method
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+	Element.prototype.closest = function(s) {
+		var el = this;
+		if (!document.documentElement.contains(el)) return null;
+		do {
+			if (el.matches(s)) return el;
+			el = el.parentElement || el.parentNode;
+		} while (el !== null && el.nodeType === 1); 
+		return null;
+	};
+}
+
+//Custom Event() constructor
+if ( typeof window.CustomEvent !== "function" ) {
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+}
+
+/* 
+	Animation curves
+*/
+Math.easeInOutQuad = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
+
+Math.easeInQuart = function (t, b, c, d) {
+	t /= d;
+	return c*t*t*t*t + b;
+};
+
+Math.easeOutQuart = function (t, b, c, d) { 
+  t /= d;
+	t--;
+	return -c * (t*t*t*t - 1) + b;
+};
+
+Math.easeInOutQuart = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t*t + b;
+	t -= 2;
+	return -c/2 * (t*t*t*t - 2) + b;
+};
+
+Math.easeOutElastic = function (t, b, c, d) {
+  var s=1.70158;var p=d*0.7;var a=c;
+  if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+  if (a < Math.abs(c)) { a=c; var s=p/4; }
+  else var s = p/(2*Math.PI) * Math.asin (c/a);
+  return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+};
+
+
+/* JS Utility Classes */
+
+// make focus ring visible only for keyboard navigation (i.e., tab key) 
+(function() {
+  var focusTab = document.getElementsByClassName('js-tab-focus'),
+    shouldInit = false,
+    outlineStyle = false,
+    eventDetected = false;
+
+  function detectClick() {
+    if(focusTab.length > 0) {
+      resetFocusStyle(false);
+      window.addEventListener('keydown', detectTab);
+    }
+    window.removeEventListener('mousedown', detectClick);
+    outlineStyle = false;
+    eventDetected = true;
+  };
+
+  function detectTab(event) {
+    if(event.keyCode !== 9) return;
+    resetFocusStyle(true);
+    window.removeEventListener('keydown', detectTab);
+    window.addEventListener('mousedown', detectClick);
+    outlineStyle = true;
+  };
+
+  function resetFocusStyle(bool) {
+    var outlineStyle = bool ? '' : 'none';
+    for(var i = 0; i < focusTab.length; i++) {
+      focusTab[i].style.setProperty('outline', outlineStyle);
+    }
+  };
+
+  function initFocusTabs() {
+    if(shouldInit) {
+      if(eventDetected) resetFocusStyle(outlineStyle);
+      return;
+    }
+    shouldInit = focusTab.length > 0;
+    window.addEventListener('mousedown', detectClick);
+  };
+
+  initFocusTabs();
+  window.addEventListener('initFocusTabs', initFocusTabs);
+}());
+
+function resetFocusTabsStyle() {
+  window.dispatchEvent(new CustomEvent('initFocusTabs'));
+};
+// File#: _1_tabs
+// Usage: codyhouse.co/license
+(function() {
+	var Tab = function(element) {
+		this.element = element;
+		this.tabList = this.element.getElementsByClassName('js-tabs__controls')[0];
+		this.listItems = this.tabList.getElementsByTagName('li');
+		this.triggers = this.tabList.getElementsByTagName('a');
+		this.panelsList = this.element.getElementsByClassName('js-tabs__panels')[0];
+		this.panels = Util.getChildrenByClassName(this.panelsList, 'js-tabs__panel');
+		this.hideClass = 'is-hidden';
+		this.customShowClass = this.element.getAttribute('data-show-panel-class') ? this.element.getAttribute('data-show-panel-class') : false;
+		this.layout = this.element.getAttribute('data-tabs-layout') ? this.element.getAttribute('data-tabs-layout') : 'horizontal';
+		// deep linking options
+		this.deepLinkOn = this.element.getAttribute('data-deep-link') == 'on';
+		// init tabs
+		this.initTab();
+	};
+
+	Tab.prototype.initTab = function() {
+		//set initial aria attributes
+		this.tabList.setAttribute('role', 'tablist');
+		for( var i = 0; i < this.triggers.length; i++) {
+			var bool = (i == 0),
+				panelId = this.panels[i].getAttribute('id');
+			this.listItems[i].setAttribute('role', 'presentation');
+			Util.setAttributes(this.triggers[i], {'role': 'tab', 'aria-selected': bool, 'aria-controls': panelId, 'id': 'tab-'+panelId});
+			Util.addClass(this.triggers[i], 'js-tabs__trigger'); 
+			Util.setAttributes(this.panels[i], {'role': 'tabpanel', 'aria-labelledby': 'tab-'+panelId});
+			Util.toggleClass(this.panels[i], this.hideClass, !bool);
+
+			if(!bool) this.triggers[i].setAttribute('tabindex', '-1'); 
+		}
+
+		//listen for Tab events
+		this.initTabEvents();
+
+		// check deep linking option
+		this.initDeepLink();
+	};
+
+	Tab.prototype.initTabEvents = function() {
+		var self = this;
+		//click on a new tab -> select content
+		this.tabList.addEventListener('click', function(event) {
+			if( event.target.closest('.js-tabs__trigger') ) self.triggerTab(event.target.closest('.js-tabs__trigger'), event);
+		});
+		//arrow keys to navigate through tabs 
+		this.tabList.addEventListener('keydown', function(event) {
+			;
+			if( !event.target.closest('.js-tabs__trigger') ) return;
+			if( tabNavigateNext(event, self.layout) ) {
+				event.preventDefault();
+				self.selectNewTab('next');
+			} else if( tabNavigatePrev(event, self.layout) ) {
+				event.preventDefault();
+				self.selectNewTab('prev');
+			}
+		});
+	};
+
+	Tab.prototype.selectNewTab = function(direction) {
+		var selectedTab = this.tabList.querySelector('[aria-selected="true"]'),
+			index = Util.getIndexInArray(this.triggers, selectedTab);
+		index = (direction == 'next') ? index + 1 : index - 1;
+		//make sure index is in the correct interval 
+		//-> from last element go to first using the right arrow, from first element go to last using the left arrow
+		if(index < 0) index = this.listItems.length - 1;
+		if(index >= this.listItems.length) index = 0;	
+		this.triggerTab(this.triggers[index]);
+		this.triggers[index].focus();
+	};
+
+	Tab.prototype.triggerTab = function(tabTrigger, event) {
+		var self = this;
+		event && event.preventDefault();	
+		var index = Util.getIndexInArray(this.triggers, tabTrigger);
+		//no need to do anything if tab was already selected
+		if(this.triggers[index].getAttribute('aria-selected') == 'true') return;
+		
+		for( var i = 0; i < this.triggers.length; i++) {
+			var bool = (i == index);
+			Util.toggleClass(this.panels[i], this.hideClass, !bool);
+			if(this.customShowClass) Util.toggleClass(this.panels[i], this.customShowClass, bool);
+			this.triggers[i].setAttribute('aria-selected', bool);
+			bool ? this.triggers[i].setAttribute('tabindex', '0') : this.triggers[i].setAttribute('tabindex', '-1');
+		}
+
+		// update url if deepLink is on
+		if(this.deepLinkOn) {
+			history.replaceState(null, '', '#'+tabTrigger.getAttribute('aria-controls'));
+		}
+	};
+
+	Tab.prototype.initDeepLink = function() {
+		if(!this.deepLinkOn) return;
+		var hash = window.location.hash.substr(1);
+		var self = this;
+		if(!hash || hash == '') return;
+		for(var i = 0; i < this.panels.length; i++) {
+			if(this.panels[i].getAttribute('id') == hash) {
+				this.triggerTab(this.triggers[i], false);
+				setTimeout(function(){self.panels[i].scrollIntoView(true);});
+				break;
+			}
+		};
+	};
+
+	function tabNavigateNext(event, layout) {
+		if(layout == 'horizontal' && (event.keyCode && event.keyCode == 39 || event.key && event.key == 'ArrowRight')) {return true;}
+		else if(layout == 'vertical' && (event.keyCode && event.keyCode == 40 || event.key && event.key == 'ArrowDown')) {return true;}
+		else {return false;}
+	};
+
+	function tabNavigatePrev(event, layout) {
+		if(layout == 'horizontal' && (event.keyCode && event.keyCode == 37 || event.key && event.key == 'ArrowLeft')) {return true;}
+		else if(layout == 'vertical' && (event.keyCode && event.keyCode == 38 || event.key && event.key == 'ArrowUp')) {return true;}
+		else {return false;}
+	};
+	
+	//initialize the Tab objects
+	var tabs = document.getElementsByClassName('js-tabs');
+	if( tabs.length > 0 ) {
+		for( var i = 0; i < tabs.length; i++) {
+			(function(i){new Tab(tabs[i]);})(i);
+		}
+	}
+}());
+// File#: _1_accordion
+// Usage: codyhouse.co/license
+(function() {
+	var Accordion = function(element) {
+		this.element = element;
+		this.items = Util.getChildrenByClassName(this.element, 'js-accordion__item');
+		this.version = this.element.getAttribute('data-version') ? '-'+this.element.getAttribute('data-version') : '';
+		this.showClass = 'accordion'+this.version+'__item--is-open';
+		this.animateHeight = (this.element.getAttribute('data-animation') == 'on');
+		this.multiItems = !(this.element.getAttribute('data-multi-items') == 'off'); 
+		// deep linking options
+		this.deepLinkOn = this.element.getAttribute('data-deep-link') == 'on';
+		// init accordion
+		this.initAccordion();
+	};
+
+	Accordion.prototype.initAccordion = function() {
+		//set initial aria attributes
+		for( var i = 0; i < this.items.length; i++) {
+			var button = this.items[i].getElementsByTagName('button')[0],
+				content = this.items[i].getElementsByClassName('js-accordion__panel')[0],
+				isOpen = Util.hasClass(this.items[i], this.showClass) ? 'true' : 'false';
+			Util.setAttributes(button, {'aria-expanded': isOpen, 'aria-controls': 'accordion-content-'+i, 'id': 'accordion-header-'+i});
+			Util.addClass(button, 'js-accordion__trigger');
+			Util.setAttributes(content, {'aria-labelledby': 'accordion-header-'+i, 'id': 'accordion-content-'+i});
+		}
+
+		//listen for Accordion events
+		this.initAccordionEvents();
+
+		// check deep linking option
+		this.initDeepLink();
+	};
+
+	Accordion.prototype.initAccordionEvents = function() {
+		var self = this;
+
+		this.element.addEventListener('click', function(event) {
+			var trigger = event.target.closest('.js-accordion__trigger');
+			//check index to make sure the click didn't happen inside a children accordion
+			if( trigger && Util.getIndexInArray(self.items, trigger.parentElement) >= 0) self.triggerAccordion(trigger);
+		});
+	};
+
+	Accordion.prototype.triggerAccordion = function(trigger) {
+		var bool = (trigger.getAttribute('aria-expanded') === 'true');
+
+		this.animateAccordion(trigger, bool, false);
+
+		if(!bool && this.deepLinkOn) {
+			history.replaceState(null, '', '#'+trigger.getAttribute('aria-controls'));
+		}
+	};
+
+	Accordion.prototype.animateAccordion = function(trigger, bool, deepLink) {
+		var self = this;
+		var item = trigger.closest('.js-accordion__item'),
+			content = item.getElementsByClassName('js-accordion__panel')[0],
+			ariaValue = bool ? 'false' : 'true';
+
+		if(!bool) Util.addClass(item, this.showClass);
+		trigger.setAttribute('aria-expanded', ariaValue);
+		self.resetContentVisibility(item, content, bool);
+
+		if( !this.multiItems && !bool || deepLink) this.closeSiblings(item);
+	};
+
+	Accordion.prototype.resetContentVisibility = function(item, content, bool) {
+		Util.toggleClass(item, this.showClass, !bool);
+		content.removeAttribute("style");
+		if(bool && !this.multiItems) { // accordion item has been closed -> check if there's one open to move inside viewport 
+			this.moveContent();
+		}
+	};
+
+	Accordion.prototype.closeSiblings = function(item) {
+		//if only one accordion can be open -> search if there's another one open
+		var index = Util.getIndexInArray(this.items, item);
+		for( var i = 0; i < this.items.length; i++) {
+			if(Util.hasClass(this.items[i], this.showClass) && i != index) {
+				this.animateAccordion(this.items[i].getElementsByClassName('js-accordion__trigger')[0], true, false);
+				return false;
+			}
+		}
+	};
+
+	Accordion.prototype.moveContent = function() { // make sure title of the accordion just opened is inside the viewport
+		var openAccordion = this.element.getElementsByClassName(this.showClass);
+		if(openAccordion.length == 0) return;
+		var boundingRect = openAccordion[0].getBoundingClientRect();
+		if(boundingRect.top < 0 || boundingRect.top > window.innerHeight) {
+			var windowScrollTop = window.scrollY || document.documentElement.scrollTop;
+			window.scrollTo(0, boundingRect.top + windowScrollTop);
+		}
+	};
+
+	Accordion.prototype.initDeepLink = function() {
+		if(!this.deepLinkOn) return;
+		var hash = window.location.hash.substr(1);
+		if(!hash || hash == '') return;
+		var trigger = this.element.querySelector('.js-accordion__trigger[aria-controls="'+hash+'"]');
+		if(trigger && trigger.getAttribute('aria-expanded') !== 'true') {
+			this.animateAccordion(trigger, false, true);
+			setTimeout(function(){trigger.scrollIntoView(true);});
+		}
+	};
+
+	window.Accordion = Accordion;
+	
+	//initialize the Accordion objects
+	var accordions = document.getElementsByClassName('js-accordion');
+	if( accordions.length > 0 ) {
+		for( var i = 0; i < accordions.length; i++) {
+			(function(i){new Accordion(accordions[i]);})(i);
+		}
+	}
+}());
+// File#: _2_tabs-v3
+// Usage: codyhouse.co/license
